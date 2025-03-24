dada0ac14534f169a45d08367c3d9dd211549036
pooja <pooja.gla_cs21@gla.ac.in>
2025-03-24 11:47:10 +0530

    message with username

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  standalone:true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Hello from BridgeLabz';
+  userName='';
+  title='Hello from BridgeLabz';
   logoUrl = 'assets/images/BridgeLabz.jpg';  // Property holding the image URL
   bridgeLabzUrl = 'https://www.bridgelabz.com';  // BridgeLabz website URL
-
-  // Method to open BridgeLabz URL in a new tab
-  openBridgeLabzSite() {
-    window.open(this.bridgeLabzUrl, '_blank');  // Opens the URL in a new tab
+  onInputChange(event: Event) {
+    this.userName = (event.target as HTMLInputElement).value;
   }
 }
