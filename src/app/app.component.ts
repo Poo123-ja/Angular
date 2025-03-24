64a40edd60a431f0f86fe1b4d5a7af33201f93d1
pooja <pooja.gla_cs21@gla.ac.in>
2025-03-24 11:36:40 +0530

    open site by clicking logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,10 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Hello from BridgeLabz';
   logoUrl = 'assets/images/BridgeLabz.jpg';  // Property holding the image URL
+  bridgeLabzUrl = 'https://www.bridgelabz.com';  // BridgeLabz website URL
+
+  // Method to open BridgeLabz URL in a new tab
+  openBridgeLabzSite() {
+    window.open(this.bridgeLabzUrl, '_blank');  // Opens the URL in a new tab
+  }
 }
