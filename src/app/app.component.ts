f49c3e854bb41b0c80b3208d94a64119db1628a4
pooja <pooja.gla_cs21@gla.ac.in>
2025-03-24 10:56:42 +0530

    Data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
+
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularIntro';
+  title = 'Hello from BridgeLabz';  // The property for interpolation
 }
